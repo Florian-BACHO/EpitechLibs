42afb8920aed13923c2ff687a400b6ce636b6b53
Florian BACHO <florian.bacho@epitech.eu>
2018-05-23 17:29:26 +0200

    Updated read / write until function to copy the stop data

--- circular_buffer/src/cbuffer_read_write.c
+++ circular_buffer/src/cbuffer_read_write.c
@@ -41,11 +41,12 @@ size_t cbuffer_write_until(circular_buffer_t *buffer, void *data,
 	va_list ap;
 
 	va_start(ap, stop_cond);
-	while (!stop_cond(data + (out * buffer->data_size), ap) &&
-		!cbuffer_is_full(buffer)) {
+	while (!cbuffer_is_full(buffer)) {
 		memcpy(buffer->buffer + (buffer->write * buffer->data_size),
 			data + (out * buffer->data_size), buffer->data_size);
 		out++;
+		if (stop_cond(data + (out * buffer->data_size), ap))
+			break;
 		buffer->write = pcb_get_next_idx(buffer->write, buffer->len);
 		va_end(ap);
 		va_start(ap, stop_cond);
@@ -61,12 +62,14 @@ size_t cbuffer_read_until(circular_buffer_t *buffer, void *data,
 	va_list ap;
 
 	va_start(ap, stop_cond);
-	while (!stop_cond(buffer->buffer + (buffer->read * buffer->data_size),
-				ap) && !cbuffer_is_empty(buffer)) {
+	while (!cbuffer_is_empty(buffer)) {
 		memcpy(data + (out * buffer->data_size),
 			buffer->buffer + (buffer->read * buffer->data_size),
 			buffer->data_size);
 		out++;
+		if (stop_cond(buffer->buffer + (buffer->read *
+						buffer->data_size), ap))
+			break;
 		buffer->read = pcb_get_next_idx(buffer->read, buffer->len);
 		va_end(ap);
 		va_start(ap, stop_cond);
