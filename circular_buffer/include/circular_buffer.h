fb5f860caf421b5f709513e7199017e2608c956f
Florian BACHO <florian.bacho@epitech.eu>
2018-05-23 16:37:02 +0200

    Added read / write until function in circular buffer library

--- circular_buffer/include/circular_buffer.h
+++ circular_buffer/include/circular_buffer.h
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdarg.h>
 
 typedef struct circular_buffer {
 
@@ -22,6 +23,8 @@ typedef struct circular_buffer {
 	size_t write;
 } circular_buffer_t;
 
+typedef bool (*condition_t)(void *, va_list);
+
 /*
 ** Public
 */
@@ -30,14 +33,25 @@ void cbuffer_reset(circular_buffer_t *buffer);
 void cbuffer_destroy(circular_buffer_t *buffer);
 bool cbuffer_is_empty(const circular_buffer_t *buffer);
 bool cbuffer_is_full(const circular_buffer_t *buffer);
+
 /*
 ** Use like write system call
 */
 size_t cbuffer_write(circular_buffer_t *buffer, void *data, size_t len);
+
 /*
 ** Use like read system call
 */
 size_t cbuffer_read(circular_buffer_t *buffer, void *data, size_t len);
+
+/*
+** Read / Write while stop condition return false
+** Stop if cannot read or write anymore
+*/
+size_t cbuffer_write_until(circular_buffer_t *buffer, void *data,
+				condition_t stop_cond, ...);
+size_t cbuffer_read_until(circular_buffer_t *buffer, void *data,
+				condition_t stop_cond, ...);
 
 /*
 ** Private (pcb == private circular_buffer)
--- circular_buffer/src/cbuffer_read_write.c
+++ circular_buffer/src/cbuffer_read_write.c
@@ -11,9 +11,7 @@ size_t cbuffer_write(circular_buffer_t *buffer, void *data, size_t len)
 {
 	size_t out = 0u;
 
-	while (out < len) {
-		if (cbuffer_is_full(buffer))
-			break;
+	while (out < len && !cbuffer_is_full(buffer)) {
 		memcpy(buffer->buffer + (buffer->write * buffer->data_size),
 			data + (out * buffer->data_size), buffer->data_size);
 		out++;
@@ -26,14 +24,53 @@ size_t cbuffer_read(circular_buffer_t *buffer, void *data, size_t len)
 {
 	size_t out = 0u;
 
-	while (out < len) {
-		if (cbuffer_is_empty(buffer))
-			break;
+	while (out < len && !cbuffer_is_empty(buffer)) {
 		memcpy(data + (out * buffer->data_size),
 			buffer->buffer + (buffer->read * buffer->data_size),
 			buffer->data_size);
 		out++;
 		buffer->read = pcb_get_next_idx(buffer->read, buffer->len);
 	}
+	return (out);
+}
+
+size_t cbuffer_write_until(circular_buffer_t *buffer, void *data,
+				condition_t stop_cond, ...)
+{
+	size_t out = 0u;
+	va_list ap;
+
+	va_start(ap, stop_cond);
+	while (!stop_cond(data + (out * buffer->data_size), ap) &&
+		!cbuffer_is_full(buffer)) {
+		memcpy(buffer->buffer + (buffer->write * buffer->data_size),
+			data + (out * buffer->data_size), buffer->data_size);
+		out++;
+		buffer->write = pcb_get_next_idx(buffer->write, buffer->len);
+		va_end(ap);
+		va_start(ap, stop_cond);
+	}
+	va_end(ap);
+	return (out);
+}
+
+size_t cbuffer_read_until(circular_buffer_t *buffer, void *data,
+				condition_t stop_cond, ...)
+{
+	size_t out = 0u;
+	va_list ap;
+
+	va_start(ap, stop_cond);
+	while (!stop_cond(buffer->buffer + (buffer->read * buffer->data_size),
+				ap) && !cbuffer_is_empty(buffer)) {
+		memcpy(data + (out * buffer->data_size),
+			buffer->buffer + (buffer->read * buffer->data_size),
+			buffer->data_size);
+		out++;
+		buffer->read = pcb_get_next_idx(buffer->read, buffer->len);
+		va_end(ap);
+		va_start(ap, stop_cond);
+	}
+	va_end(ap);
 	return (out);
 }
